a21c6dde6d7656a07e96366ae65b0c40c43aa798
biaobiaoqi <shenyapeng@gmail.com>
2013-10-31 18:31:35 +0800

    Update README

--- fire.rb
+++ fire.rb
@@ -1,5 +1,5 @@
 require './PostMachine.rb'
 
-	tboard = TargetBoard.new('100', '4273163', '4273163')
+	tboard = TargetBoard.new('182', '4273572', '4273572')
 	cc98_post_machine = PostMachine.new('www.cc98.org', tboard)
 	cc98_post_machine.send_posts
